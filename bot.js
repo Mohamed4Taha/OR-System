b2a3e7024e2d1c61ad5a9e6d07fc3a4a01acf799
Mohamed4Taha <44267537+Mohamed4Taha@users.noreply.github.com>
2018-11-08 20:49:51 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -35,6 +35,13 @@ client.login(process.env.BOT_TOKEN);
 
 client.on('message', msg => {
     if (msg.content === 'برب') {
+       msg.reply('=Discord : - OR | Your Name
+=Game Play : ๖ۣۜǾŘ༻ | Your Name');
+      }
+    });
+
+client.on('message', msg => {
+    if (msg.content === 'برب') {
        msg.reply('**تيت ي عسڵ , ڵٱ تطوڵ :broken_heart: **');
       }
     });
